f8e992f3bed0cb56ba8943936257c30b75b7f296
uhhdirt <paulwalkercares@gmail.com>
2026-06-19 09:00:28 -0700

    Grow App addition

---
+++ grow-app/src/main.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import GrowTracker from './GrowTracker.jsx';
+import './index.css';
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode><GrowTracker /></React.StrictMode>
+);
