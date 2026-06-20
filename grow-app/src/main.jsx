d7e33ed74c0fffdf9b1f7e142caa19066e4105ae
uhhdirt <paulwalkercares@gmail.com>
2026-06-20 15:51:32 -0700

    Delete grow-app directory

--- grow-app/src/main.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import GrowTracker from './GrowTracker.jsx';
-import './index.css';
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode><GrowTracker /></React.StrictMode>
-);
